dbe483c1ec8289457457fc1bf390d0d785c95df1
Ihor Maslovskyi <147808182+Maslowww@users.noreply.github.com>
2025-09-19 09:54:54 +0100

    Lab1 finished

---
+++ pr_2.py
@@ -0,0 +1,3 @@
+x = input(" What's your Number 1?")
+y = input(" What's your Number 2?")
+print("Your numbers are, " + x + " and " + y )
---
+++ pr_3.py
@@ -0,0 +1,4 @@
+name = input(" What's your name?")
+age = input(" How old are you?")
+student_id = input(" What's your Student ID?")
+print(" Your name is " + name + " you are " + age + " years old." + " your student id is " + student_id)
